2befa2599a4aa05a639dede491bdcf561a540d7e
baruaee <email-address>
2023-10-12 19:28:39 +0200

    misc.

--- MyProject/1_Install_ngspice.sh
+++ MyProject/1_Install_ngspice.sh
@@ -3,9 +3,5 @@
 
 # Install Ngspice
 
-sudo apt update
-sudo apt upgrade
 sudo apt-get install manpages-dev man-db manpages-posix-dev libreadline6-dev -y
-sudo apt-get update -y
-
 sudo apt install ngspice
