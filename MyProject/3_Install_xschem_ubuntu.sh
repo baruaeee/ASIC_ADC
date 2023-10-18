b106a9862841d6ecb5decbfdd6d55b5f593df8b7
baruaeee <s.b_04@yahoo.com>
2023-10-18 15:28:11 +0200

    misc.

--- MyProject/3_Install_xschem_ubuntu.sh
+++ MyProject/3_Install_xschem_ubuntu.sh
@@ -2,7 +2,7 @@
 
 # Install Xschem
 #sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean -y && sudo apt autoclean -y
-sudo apt-get install -y git build-essential libx11-dev libxaw7-dev libcairo2-dev tcl8.6-dev tk8.6-dev flex bison libxpm-dev gawk adms libreadline-dev
+sudo apt-get install -y git build-essential libx11-dev libxaw7-dev libcairo2-dev tcl8.6-dev tk8.6-dev flex bison libxpm-dev gawk adms libreadline-dev xterm
 git clone https://github.com/StefanSchippers/xschem.git
 cd xschem
 ./configure
