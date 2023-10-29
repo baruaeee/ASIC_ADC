bc1d77db206a2d675421b7ee127518e7bfa9817d
baruaee <email-address>
2023-10-29 17:45:30 +0100

    Semi auto ngspice simulation with python script tested

--- xschem/Vth_sch/test_ngspice/test.py
+++ xschem/Vth_sch/test_ngspice/test.py
@@ -1,30 +1,18 @@
-import subprocess
+# include the function 'ng_run' in NG_RUN.py file
+# from ipynb.fs.full.NG_LIB import ng_run, read_PL, read_PW, read_NL, read_NW, write_PL, write_PW, write_vin
 
-# Run the ngspice command
-#ngspice_command = ['ngspice', '-i', '-a', 'res-script.spice']
-ngspice_command = ['ngspice', '-i', '-a', 'test_script.spice']
-ngspice_process = subprocess.Popen(ngspice_command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+from NG_LIB import ng_run, read_PL, read_PW, read_NL, read_NW, write_PL, write_PW, write_vin
 
-# Pass a command to the ngspice console
-command_to_ngspice = 'print vout\n'
-ngspice_process.stdin.write(command_to_ngspice)
-#command_to_ngspice1 = 'print all\n'
-#ngspice_process.stdin.write(command_to_ngspice1)
-ngspice_process.stdin.flush()
+# prompt for selecting the netlist file
+file_path = input("Enter file name: ")
 
-# Read the output from ngspice
-# Read the output from ngspice
-output, error = ngspice_process.communicate()
 
-# Read the output and extract the desired data
-lines = output.splitlines()
-line_index = [index for index, line in enumerate(lines) if line.startswith('vout')]
-print(line_index)
-desired_line = lines[line_index[0]]  # Assuming the desired line is at index 19
-print("Desired data line:", desired_line)
+pfet_Width=read_PW(file_path)
+print(pfet_Width)
+write_PW(file_path, str(pfet_Width), '25')
 
-#Extract the specific data from the desired line
-data = desired_line.split('=')[1].strip()  # Assuming the data is after the '='
-
-print("Desired data:", float(data))
-
+'''
+pfet_Length=read_PL(file_path)
+print(pfet_Length)
+write_PL(file_path, str(pfet_Length), '0.15')
+'''
