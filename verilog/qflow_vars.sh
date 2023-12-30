e956a5ea3822a6d47c11d0ad6341be12a0fe469d
baruaee <email-address>
2023-12-30 23:37:26 +0100

    Openlane result added

---
+++ verilog/qflow_vars.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/tcsh -f
+#-------------------------------------------
+# qflow variables for project /home/exotic/Desktop/ASIC_ADC/verilog
+#-------------------------------------------
+
+set projectpath=/home/exotic/Desktop/ASIC_ADC/verilog
+set techdir=/usr/share/qflow/tech/sky130_fd_sc_hd
+set sourcedir=/home/exotic/Desktop/ASIC_ADC/verilog/source
+set synthdir=/home/exotic/Desktop/ASIC_ADC/verilog/synthesis
+set layoutdir=/home/exotic/Desktop/ASIC_ADC/verilog/layout
+set techname=sky130_fd_sc_hd
+set scriptdir=/usr/lib/qflow/scripts
+set bindir=/usr/lib/qflow/bin
+set logdir=/home/exotic/Desktop/ASIC_ADC/verilog/log
+#-------------------------------------------
+
